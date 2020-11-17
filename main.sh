a8e4d562172c4f9861be1ef42b93ca01dce1a2e7
Samthebest999 <samit.mohnot.018@k12.friscoisd.org>
2020-11-17 20:56:53 +0000

    Initial commit

---
+++ main.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+echo "So, You want to set up a LAMP(Linux Apache MySQL Php) Stack/Server?"
+echo "Well, you've come to the right place..."
+echo "Loading."
+sleep 5
+echo "Loading.."
+sleep 5
+echo "Loading..."
+echo "Done!!"
+echo "Would you like to continue with the installation? Y/N (case sensitive)"
+read continue
+if [ $continue = "Y" ]
+then
+    sudo apt update && sudo apt full-upgrade
+    sudo apt install apache2 mysql php -y
+    install-pkg apache2 mysql php
+fi
+echo "Done!!"
